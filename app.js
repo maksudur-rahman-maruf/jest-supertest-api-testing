afacabc2349daad0d4f77ed522c0d7c7fb0f56ee
maksudur-rahman-maruf <maruf.bsnm@gmail.com>
2022-01-10 19:06:02 +0600

    first-commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express =  require('express');
+
+const app = express();
+
+app.use(express.json());
+app.post('/users', async (req, res) => {
+  const { password, username } = req.body;
+  if (!password || !username) {
+    res.sendStatus(400);
+    return;
+  }
+
+  res.send({ userId: 0 });
+})
+
+module.exports = app;
